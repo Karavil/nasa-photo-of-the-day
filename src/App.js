e3c6975896ffcee906501a783b2882789cf5e6a9
Alex Karavil <alex@karavil.io>
2020-02-19 15:53:06 -0500

    Added support for adding multiple image-of-the-days to the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import NasaCard from "./components/NasaCard";
 
-import { NASA_API_KEY } from "./config.js"; 
+import { NASA_API_KEY } from "./config.js";
 
 import "./App.css";
 
 const App = () => {
-   const [nasaImages, setNasaImages] = useState([]);
+   const [images, setImages] = useState([]);
+   const NASA_API = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`
+
+   const generateRandomDates = (count) => {
+      return ['2012-01-01','2011-01-01','2010-01-01','2009-01-01']
+   }
 
    useEffect(() => {
-      
-   }, [nasaImages])
-
-   const NASA_API = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`
+      generateRandomDates(10).forEach(date => {
+         console.log(date);
+         axios.get(NASA_API, {
+            params: {
+               hd: true,
+               date: date
+            }
+         })
+            .then(function (response) {
+               // handle success
+               images.push(response.data);
+               setImages([...images]);
+            })
+            .catch(function (error) {
+               // handle error
+               console.log('Error', error);
+            })
+      })
+   }, [])
 
    return (
       <div className="App">
          {
-
+            images.map((image) => {
+               return (
+                  <NasaCard key={image.date} title={image.title} info={image.explanation} date={image.date} imgSrc={image.hdurl} />
+               )
+            })
+         }
+         {
+            console.log(images)
          }
       </div>
    );
--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './NasaCard.css'
 
 const NasaCard = props => {
    return (
-      <div class="card">
+      <div className="card">
          <img src={props.imgSrc} alt='An image from NASA. A different image provided each day (mostly pertaining to space exploration)'></img>
          <h2>{props.title}</h2>
          <h3>{props.date}</h3>
